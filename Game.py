0ab126d12dbf7b5d85c7880585f91f464b921aae
Mark Putnam <70597443+PutnamM3196@users.noreply.github.com>
2021-10-20 14:12:56 -0400

    Add files via upload

---
+++ Game.py
@@ -0,0 +1,155 @@
+# CSC 221
+# Text Adventure
+# Mark Putnam
+# 10/1/21
+
+from Room import Room
+from Player import Player
+
+
+class Game:
+
+    def __init__(self):
+        """ Initialize object (with no rooms) """
+        self.rooms = { } 
+        self.player = Player() 
+        
+        self.isPlaying = True
+        self.isVerbose = True # auto-look on move
+        
+
+    def __str__(self):
+        pass
+
+    def __repr__(self):
+        pass
+
+    def output(self, message):
+        """ output the message. Just uses print() in base class.
+        You might for example subclass to use Flask, etc. """
+        print(message)
+
+    def setup(self):
+        """ setup(): create a graph of rooms for play. """
+        
+
+    def loop(self):
+        """ loop(): the main game loop.
+        Continues until the user quits. """
+        self.isPlaying = True
+        while self.isPlaying:
+            self.playerAction()
+        print("Game over, thanks for playing")
+        
+
+
+    def end(self):
+        """ finish game, inform user of score and turns played. """
+        pass
+    
+    def playerAction(self):
+        """ Ask user for input, validate it, update the game state. """
+        command = input(">")
+        command = command.lower()
+        words = command.split() # split on whitespace
+        if len(words) < 1:
+            print("No input detected")
+            return
+        
+        verb = words[0]
+        if verb == "go":
+            direction = words[1]
+            self.commandGo(direction)    
+        elif verb == "look":
+            self.here.describe()
+        elif verb == "quit":
+            self.isPlaying = False
+            print("quitting")
+        elif verb == "get":
+            item = words[1]
+            self.commandGet(item)
+        elif verb == "drop":
+            item = words[1]
+            self.commandDrop(item)
+        elif verb == "help":
+            print("""
+go [direction] -- Moves your character in that direction (ex. North)
+look -- displays where you are
+get [item] -- grabs the item
+drop [item] -- drops the item
+quit -- exits the game
+""")
+        
+        else: # first word is verb
+            print("I don't know how to", words[0])
+
+    def commandGo(self, direction):
+        """ 
+        input: direction to move.
+        output: none
+        side effect: player location is updated if possible.
+        """
+        # Can we go in the chosen direction from here?
+        if self.here.exits.get(direction) == None:
+            print("You can't go that way.")
+        else:   
+            # this key does exist
+            newRoomName = self.here.exits[direction]
+            newRoom     = self.rooms[newRoomName]
+            self.here   = newRoom
+            if self.isVerbose:
+                self.here.describe()
+    
+    def commandGet(self, itemName):
+        """ remove the item from the room (if it's there)
+        and place it in player inventory.
+        """
+        # TODO: actually do this
+        # We'll need to remove the item from the current
+        # room, and then add it to the player inventory
+        # (which means we need a player inventory)
+        print("You try to get the", itemName)
+        # THIS IS BROKEN
+        # until Container is fixed. (SEE CONTAINER.PY)
+        
+        if self.here.contains(itemName):
+            item = self.here.contents[itemName]
+            self.here.moveItemTo(item, self.player)
+            print("You pick up the",itemName,".")
+        else:
+            print("You can't see any", itemName, "here.")
+        
+     
+    def commandDrop(self, itemName):
+        """ remove the item from player inventory
+        (if it's there) and add it to the room. 
+        """
+        print("You try to drop the", itemName)
+        
+        if self.player.contains(itemName):
+            item = self.player.contents[itemName]
+            self.player.moveItemTo(item, self.here)
+            print("You drop the", itemName,".")
+        else:
+            print("You don't have a",itemName,".")
+            
+
+    # Helper functions -- not necessary, but useful
+    @property
+    def here(self):
+        return self.player.loc
+    
+    @here.setter
+    def here(self, room):
+        self.player.loc = room
+
+def main():
+    game = Game()
+    game.setup()
+    print("Starting game -- enter command.")
+    game.loop()
+    game.end()
+
+
+if __name__ == "__main__":
+    main()
---
+++ MyGame.py
@@ -0,0 +1,84 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Mon Oct 11 13:21:09 2021
+
+@author: putnamm3196
+"""
+
+from Game import Game
+from Room import Room
+from Player import Player
+from Item import Item
+
+class MyGame(Game):
+    def setup(self):
+        loader = MyGameLoader()
+        self.rooms = loader.setup()
+        self.here = self.rooms["Hub"]
+        self.here.describe()
+        
+        
+class MyGameLoader: 
+    def setup(self):
+        """ setup(): create a graph of rooms for play. """
+        # just a test -- needs work
+
+        dungeonEntrance = Room( "Dungeon Entrance", 
+                   "",
+                   { "east": "Hub",
+                    "north": "Lobby"} )
+        dungeonLobby = Room( "Lobby",
+                            "",
+                            {"south": "Dungeon Entrance",
+                             "down": "Basement"})
+        
+        dungeonBasement = Room ( "Basement",
+                          "",
+                          {"up": "Lobby"})
+        tournament = Room ( "Tournament",
+                           "",
+                           { "north" : "Hub"} )
+        
+        weaponShop = Room ( "Weapon Shop", 
+                         "",
+                         { "south" : "Hub" } )
+        
+        
+        bossArena = Room ( "Boss Arena",
+                       "",
+                       {"west" : "Hub"})
+        hub = Room ( "Hub",
+                    " ",
+                    {"north" : "Weapon Shop",
+                     "south" : "Tournament",
+                     "east" : "Boss Arena",
+                     "west": "Dungeon Entrance"})
+        
+        # Place rooms in a dictionary.
+
+        rooms = {dungeonEntrance.name: dungeonEntrance,
+                    dungeonLobby.name: dungeonLobby,
+                    dungeonBasement.name: dungeonBasement,
+                    tournament.name: tournament,
+                    weaponShop.name: weaponShop,
+                    bossArena.name: bossArena,
+                    hub.name: hub}
+        
+        phone = Item("phone","a mysterious device, probably should be thrown away.")
+        hub.addItem(phone)
+    
+        sword = Item("sword","the best sword from the local blacksmith")
+        weaponShop.addItem(sword)
+        
+        return rooms
+    
+def main():
+    game = MyGame()
+    game.setup()
+    game.output("Starting game -- type help if needed  -- enter command.")
+    game.loop()
+    game.end()
+
+
+if __name__ == "__main__":
+    main()
